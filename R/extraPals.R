22cfae75373303ed5ca7da799807ab2559c1dad9
Tim Lucas <timcdlucas@gmail.com>
2016-06-05 09:54:45 +0100

    Extra extraPals added.

---
+++ R/extraPals.R
@@ -0,0 +1,21 @@
+
+
+
+
+extraPals <- list(
+               # http://bulbapedia.bulbagarden.net/wiki/File:Team_Rocket_party_Original_series.png
+               teamrocket = c(pokepal('meowth')[12], 
+                              pokepal('lickitung')[1], 
+                              pokepal('victreebel')[c(2, 4)], 
+                              pokepal('arbok')[1],
+                              pokepal('weezing')[8]),
+           startersPairs  = c(pokepal('venusaur')[c(1, 2)],
+                              pokepal('charizard')[c(3, 2)],
+                              pokepal('blastoise')[c(2, 3)]),
+           starterDark    = c(pokepal('ivysaur')[5],
+                              pokepal('blastoise')[1],
+                              pokepal('wartortle')[c(2, 11)],                              
+                              #pokepal('squirtle')[3],
+                              pokepal('charmeleon')[c(7, 9)])
+              )
+  
